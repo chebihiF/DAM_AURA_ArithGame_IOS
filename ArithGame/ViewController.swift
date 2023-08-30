a27d37572f776847e27e0f75ebf10125a7ae985f
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-08-30 15:18:56 +0100

    V1.0

--- ArithGame/ViewController.swift
+++ ArithGame/ViewController.swift
@@ -9,9 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nbr2Label: UILabel!
+    @IBOutlet weak var nbr1Label: UILabel!
+    
+    @IBOutlet weak var resp3Button: UIButton!
+    @IBOutlet weak var resp2Button: UIButton!
+    @IBOutlet weak var resp1Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let n1 = Int.random(in: 1..<201)
+        let n2 = Int.random(in: 1..<201)
+        
+        let resp = n1 + n2
+        
+        nbr1Label.text = String(n1)
+        nbr2Label.text = String(n2)
+        
+        let resp1 = Int.random(in: 2..<401)
+        let resp2 = Int.random(in: 2..<401)
+        let resp3 = Int.random(in: 2..<401)
+        
+        let i = Int.random(in: 1..<4)
+        
+        if i == 1 {
+            resp1Button.setTitle(String(resp), for: .normal)
+            resp2Button.setTitle(String(resp2), for: .normal)
+            resp3Button.setTitle(String(resp3), for: .normal)
+        }else if i == 2
+        {
+            resp1Button.setTitle(String(resp1), for: .normal)
+            resp2Button.setTitle(String(resp), for: .normal)
+            resp3Button.setTitle(String(resp3), for: .normal)
+        }else {
+            resp1Button.setTitle(String(resp1), for: .normal)
+            resp2Button.setTitle(String(resp2), for: .normal)
+            resp3Button.setTitle(String(resp), for: .normal)
+        }
+        
+        
     }
 
 
